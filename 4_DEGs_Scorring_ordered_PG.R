58f68bc9b758b1fc10ee94cec65e311100c07e5f
Alper Kucukural <alper.kucukural@umassmed.edu>
2026-04-21 18:46:46 -0400

    Update 4_DEGs_Scorring_ordered_PG.R

--- 4_DEGs_Scorring_ordered_PG.R
+++ 4_DEGs_Scorring_ordered_PG.R
@@ -10,7 +10,7 @@ set.seed(1234)
 options(stringsAsFactors = FALSE)
 
 # ----- Paths (EDIT THESE) ----------------------------------------------------------
-project_root <- "/Users/Paula/Desktop/Wei_2023_Analysis"
+project_root <- "{Your_Folder}"
 # Excel inputs:
 deg_path      <- file.path(project_root, "Clayton_DEG_List.xlsx")    # EDIT if stored elsewhere
 article_path  <- file.path(project_root, "Article_DEG_list.xlsx")    # EDIT if stored elsewhere
